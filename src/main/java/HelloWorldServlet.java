a306836cfb8839e197f4fd2fa76c76f439aa9e02
Tomas Hipolito <tlhipolito67@gmail.com>
2017-10-16 16:48:57 -0500

    -

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    //                            these are both classes the first is request and then response
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
+        String a = "";
+        a = (req.getParameter("name"));
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        if (a == null) {
+            out.println("<h1> Hello World!</h1>");
+        } else {
+            out.println("<h1> Hello" + a + "!</h1>");
+        }
     }
+
+
+
 }
