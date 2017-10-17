98b06ac2fd2ddc07c7f92e183fd0286dfa5a974e
Tomas Hipolito <tlhipolito67@gmail.com>
2017-10-17 09:41:15 -0500

    -added the counter to track site traffic -added the reset to reset the counter for the site -

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,8 +4,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 public class HelloWorldServlet extends HttpServlet {
-    //                            these are both classes the first is request and then response
+    // these are both classes the first is request and then response
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
         String a = "";
--- src/main/java/pageViewCounter.java
+++ src/main/java/pageViewCounter.java
@@ -1,5 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "pageViewCounter", urlPatterns = "/page")
 public class pageViewCounter extends HttpServlet {
+        int counter = 0;
 
+        protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response
+    )throws ServletException, IOException{
+            if(request.getParameter("reset") != null){
+                counter = 0;
+            }
+
+        counter++;
+
+        response.getWriter().print(
+                "<h1>You have visited this page " + counter + " times</h1>"
+        );
+    }
 }
